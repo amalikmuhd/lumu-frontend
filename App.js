c35fc18065ce33dee9cf7e2646948a071e5e9f03
codewithmalik <codewithmalik.dev@gmail.com>
2023-01-11 05:02:37 +0100

    update

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-// import { AppLoading } from 'expo';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
@@ -18,8 +17,6 @@ export default function App() {
     const user = await authStorage.getUser();
     if (user) setUser(user);
   };
-
-  // if (!isReady) return <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
