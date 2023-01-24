bc28116be05abc6c9e626a48f5476636d6f4ca37
codewithmalik <codewithmalik.dev@gmail.com>
2023-01-24 05:03:05 +0100

    update

--- App.js
+++ App.js
@@ -11,12 +11,12 @@ import { navigationRef } from './app/navigation/rootNavigation';
 
 export default function App() {
   const [user, setUser] = useState();
-  const [isReady, setIsReady] = useState(false);
+  // const [isReady, setIsReady] = useState(false);
 
-  const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) setUser(user);
-  };
+  // const restoreUser = async () => {
+  //   const user = await authStorage.getUser();
+  //   if (user) setUser(user);
+  // };
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
