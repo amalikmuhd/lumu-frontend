c9b09889ae86791e89fcf974e2e756fb67ccc0e9
codewithmalik <codewithmalik.dev@gmail.com>
2023-07-18 05:06:08 +0100

    update

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useEffect } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
 
-import ActivityIndicator from "../components/ActivityIndicator";
-import Button from "../components/Button";
-import Card from "../components/Card";
-import colors from "../config/colors";
-import listingsApi from "../api/listings";
-import routes from "../navigation/routes";
-import Screen from "../components/Screen";
-import AppText from "../components/Text";
-import useApi from "../hooks/useApi";
+import ActivityIndicator from '../components/ActivityIndicator';
+import Button from '../components/Button';
+import Card from '../components/Card';
+import colors from '../config/colors';
+import listingsApi from '../api/listings';
+import routes from '../navigation/routes';
+import Screen from '../components/Screen';
+import AppText from '../components/Text';
+
+import useApi from '../hooks/useApi';
 
 function ListingsScreen({ navigation }) {
   const getListingsApi = useApi(listingsApi.getListings);
@@ -34,7 +35,7 @@ function ListingsScreen({ navigation }) {
           renderItem={({ item }) => (
             <Card
               title={item.title}
-              subTitle={"$" + item.price}
+              subTitle={'$' + item.price}
               imageUrl={item.images[0].url}
               onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
               thumbnailUrl={item.images[0].thumbnailUrl}
