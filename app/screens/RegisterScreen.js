d540f2173079956392dc1be947e53e5c7ad248d7
codewithmalik <codewithmalik.dev@gmail.com>
2023-07-19 05:06:37 +0100

    update

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,24 +1,19 @@
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import * as Yup from "yup";
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
 
-import Screen from "../components/Screen";
-import usersApi from "../api/users";
-import authApi from "../api/auth";
-import useAuth from "../auth/useAuth";
-import {
-  ErrorMessage,
-  Form,
-  FormField,
-  SubmitButton,
-} from "../components/forms";
-import useApi from "../hooks/useApi";
-import ActivityIndicator from "../components/ActivityIndicator";
+import Screen from '../components/Screen';
+import ActivityIndicator from '../components/ActivityIndicator';
+import usersApi from '../api/users';
+import authApi from '../api/auth';
+import useAuth from '../auth/useAuth';
+import { ErrorMessage, Form, FormField, SubmitButton } from '../components/forms';
+import useApi from '../hooks/useApi';
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(4).label("Password"),
+  name: Yup.string().required().label('Name'),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password'),
 });
 
 function RegisterScreen() {
@@ -33,16 +28,13 @@ function RegisterScreen() {
     if (!result.ok) {
       if (result.data) setError(result.data.error);
       else {
-        setError("An unexpected error occurred.");
+        setError('An unexpected error occurred.');
         console.log(result);
       }
       return;
     }
 
-    const { data: authToken } = await loginApi.request(
-      userInfo.email,
-      userInfo.password
-    );
+    const { data: authToken } = await loginApi.request(userInfo.email, userInfo.password);
     auth.logIn(authToken);
   };
 
@@ -50,18 +42,9 @@ function RegisterScreen() {
     <>
       <ActivityIndicator visible={registerApi.loading || loginApi.loading} />
       <Screen style={styles.container}>
-        <Form
-          initialValues={{ name: "", email: "", password: "" }}
-          onSubmit={handleSubmit}
-          validationSchema={validationSchema}
-        >
+        <Form initialValues={{ name: '', email: '', password: '' }} onSubmit={handleSubmit} validationSchema={validationSchema}>
           <ErrorMessage error={error} visible={error} />
-          <FormField
-            autoCorrect={false}
-            icon="account"
-            name="name"
-            placeholder="Name"
-          />
+          <FormField autoCorrect={false} icon="account" name="name" placeholder="Name" />
           <FormField
             autoCapitalize="none"
             autoCorrect={false}
